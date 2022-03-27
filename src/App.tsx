81b2beecc52e0ad1380f912ada0749cf7625aa3b
giriraj nagar <giriraj.nagar@mtap.in>
2022-03-28 01:04:14 +0530

    initial comit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={require("./assets/code.gif")} className="App-logo" alt="logo" />
+        <h3>
+          giri.website( Demo for large and Small Bussinesses Ecom )
+        </h3>
+        <h1>
+          Coming Soon
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/root-components/root.component.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const RootComponent = () => {
+
+    return (
+        <div>
+            <p>Hello Root Is Here</p>
+        </div>
+    )
+}
